a1da1d32a735e4ee8af6d1112ee1f9fe7c98ab55
Rafael Kueng <rafik@physik.uzh.ch>
2017-01-30 18:06:56 +0100

    add maria

--- pages/committees.php
+++ pages/committees.php
@@ -24,6 +24,7 @@
     <li><b>Philippe Jetzer</b></li>
     <li>Yannick Bötzel</li>
     <li>Carmelina Genovese</li>
+    <li>Maria Haney</li>
     <li>Rafael Küng</li>
     <li>Lionel Philippoz</li>
     <li>Andreas Schärer</li>

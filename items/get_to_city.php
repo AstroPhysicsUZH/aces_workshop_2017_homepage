98fc9c3041aa1a1c4c3cf4a56e4d4058e376c533
Rafael Kueng <rafik@physik.uzh.ch>
2017-06-14 17:49:36 +0200

    small updates

--- items/get_to_city.php
+++ items/get_to_city.php
@@ -3,9 +3,6 @@
     Use train <span class='zvvlinie ic'>ICN</span> <span class='zvvlinie ic'>IC</span> <span class='zvvlinie ic'>IR</span> <span class='zvvlinie sb'>S2</span> <span class='zvvlinie sb'>S16</span> to get to the city centre / main station (approx 10min, station <span class="zvvstation">Zurich HB</span>).
     Alternatively you can leave at <span class="zvvstation">Oerlikon</span> and take tram <span class='zvvlinie l10'>10</span> or <span class='zvvlinie l14'>14</span> to get directly to the Irchel campus (approx 20min, station <span class="zvvstation">Milchbuck</span>).
 </p>
-<p class="warning">
-    The conference ticket is only valid from Monday to Friday and in the city zone, NOT for the airport transfer.
-</p>
 <p>
     Make sure you have the public transport ticket ready in case of a ticket control.
     For the transfer, please get a ticket for <b>3 zones before</b> before entering the tram/train
